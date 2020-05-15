40caec3339e61ac2af47b1dab84004948fc158df
zekaio <zekailing@gmail.com>
2020-05-15 22:55:32 +0800

    获取用户信息

--- resources/info.py
+++ resources/info.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from flask import session
 from flask_restful import Resource
+from common.database import get_user_info
 
 
 class Info(Resource):
     # 获取用户信息
     def get(self):
-        return "get info"
+        return get_user_info(session['open_id'])
