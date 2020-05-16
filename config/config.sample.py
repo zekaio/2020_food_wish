a43f90f0fae7cfed4cfac816065c00a7d8515651
zekaio <zekailing@gmail.com>
2020-05-16 23:21:10 +0800

    修改保存图片位置

--- config/config.sample.py
+++ config/config.sample.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-import logging
-
-
 class DATABASE(object):
     USERNAME = ''  # 用户名
     PASSWORD = ''  # 密码
@@ -14,11 +11,6 @@ class BASECONFIG(object):
     IMG_DIR = './square/'  # 图片位置，相对前端根目录
     BEGIN = "2020-5-23 0:00:00"  # 活动开始时间
     END = "2020-5-30 0:00:00"  # 活动结束时间
-
-
-class LOG(object):
-    FORMATTER = logging.Formatter('%(asctime)s\n%(message)s\n\n', datefmt='%Y-%m-%d,%H:%M:%S')
-    DATABASE_ERROR_LOG = './logs/db_error.log'  # 数据库错误日志位置
 
 
 class APPCONFIG(object):
--- resources/playground/square.py
+++ resources/playground/square.py
@@ -21,31 +21,31 @@ class Square(Resource):
     def post(self):
         data: dict = request.get_json(force=True)
         pics_path = data.get('pics_id') or []
-        # pics_path = list()
-        # if data.get('pics_id'):
-        #     for pic_id in data['pics_id']:
-        #         try:
-        #             res = requests.get(
-        #                 'https://hemc.100steps.net/2017/wechat/Home/Public/getMedia?media_id=%s' % pic_id, timeout=30)
-        #             t = json.loads(res.text)
-        #         except:
-        #             current_app.logger.error(traceback.format_exc())
-        #             abort(412, message="上传图片失败")
-        #             return
-        #         if t['status'] == 0:
-        #             try:
-        #                 pic_path = config.BASECONFIG.IMG_DIR + hashlib.md5(
-        #                     str(pic_id).encode('utf-8')).hexdigest() + '.jpg'
-        #                 pics_path.append(pic_path)
-        #                 with open(pic_path, 'wb') as f:
-        #                     f.write(base64.b64decode(t['data']))
-        #             except Exception:
-        #                 current_app.logger.error(traceback.format_exc())
-        #                 abort(412, message="服务器保存图片失败")
-        #                 return
-        #         else:
-        #             current_app.logger.error(t['errMsg'])
-        #             abort(412, message="服务器下载图片失败")
-        #             return
+        pics_path = list()
+        if data.get('pics_id'):
+            for pic_id in data['pics_id']:
+                try:
+                    res = requests.get(
+                        'https://hemc.100steps.net/2017/wechat/Home/Public/getMedia?media_id=%s' % pic_id, timeout=30)
+                    t = json.loads(res.text)
+                except:
+                    current_app.logger.error(traceback.format_exc())
+                    abort(412, message="上传图片失败")
+                    return
+                if t['status'] == 0:
+                    try:
+                        pic_path = config.BASECONFIG.IMG_DIR + hashlib.md5(
+                            str(pic_id).encode('utf-8')).hexdigest() + '.jpg'
+                        pics_path.append(pic_path)
+                        with open('.'+pic_path, 'wb') as f:
+                            f.write(base64.b64decode(t['data']))
+                    except Exception:
+                        current_app.logger.error(traceback.format_exc())
+                        abort(412, message="服务器保存图片失败")
+                        return
+                else:
+                    current_app.logger.error(t['errMsg'])
+                    abort(412, message="服务器下载图片失败")
+                    return
         send_post(dict(open_id=session['open_id'], content=data['content']), pics_path)
         return
